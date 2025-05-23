6be4d779100ddace816e8e5a61a15d0cb8c61155
sujalchavan03 <sujalchavan1220@gmail.com>
2025-05-23 23:24:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,196 @@
+// Function to open the modal
+function openModal() {
+    document.getElementById('myModal').style.display = "block";
+  }
+
+  // Function to close the modal
+  function closeModal() {
+    document.getElementById('myModal').style.display = "none";
+  }
+
+  // Close the modal if clicked outside of it
+  window.onclick = function(event) {
+    if (event.target == document.getElementById('myModal')) {
+      closeModal();
+    }
+  }
+
+  // Function to open the modal c++ 
+ 
+  function openModal1() {
+    document.getElementById('myModal1').style.display = "block";
+  }
+
+  // Function to close the modal
+  function closeModal1() {
+    document.getElementById('myModal1').style.display = "none";
+  }
+
+  // Close the modal if clicked outside of it
+  window.onclick = function(event) {
+    if (event.target == document.getElementById('myModal1')) {
+      closeModal1();
+    }
+  }
+
+  // Function to open the modal java + adv java 
+ 
+  function openModal2() {
+    document.getElementById('myModal2').style.display = "block";
+  }
+
+  // Function to close the modal
+  function closeModal2() {
+    document.getElementById('myModal2').style.display = "none";
+  }
+
+  // Close the modal if clicked outside of it
+  window.onclick = function(event) {
+    if (event.target == document.getElementById('myModal2')) {
+      closeModal2();
+    }
+  }
+
+  // Function to open the modal python +adv python 
+ 
+  function openModal3() {
+    document.getElementById('myModal3').style.display = "block";
+  }
+
+  // Function to close the modal
+  function closeModal3() {
+    document.getElementById('myModal3').style.display = "none";
+  }
+
+  // Close the modal if clicked outside of it
+  window.onclick = function(event) {
+    if (event.target == document.getElementById('myModal3')) {
+      closeModal3();
+    }
+  }
+
+  // Function to open the modal mysql dbms 
+ 
+  function openModal4() {
+    document.getElementById('myModal4').style.display = "block";
+  }
+
+  // Function to close the modal
+  function closeModal4() {
+    document.getElementById('myModal4').style.display = "none";
+  }
+
+  // Close the modal if clicked outside of it
+  window.onclick = function(event) {
+    if (event.target == document.getElementById('myModal4')) {
+      closeModal4();
+    }
+  }
+
+  // Function to open the modal html css 
+ 
+  function openModal5() {
+    document.getElementById('myModal5').style.display = "block";
+  }
+
+  // Function to close the modal
+  function closeModal5() {
+    document.getElementById('myModal5').style.display = "none";
+  }
+
+  // Close the modal if clicked outside of it
+  window.onclick = function(event) {
+    if (event.target == document.getElementById('myModal5')) {
+      closeModal5();
+    }
+  }
+
+  // Function to open the modal javascript 
+ 
+  function openModal6() {
+    document.getElementById('myModal6').style.display = "block";
+  }
+
+  // Function to close the modal
+  function closeModal6() {
+    document.getElementById('myModal6').style.display = "none";
+  }
+
+  // Close the modal if clicked outside of it
+  window.onclick = function(event) {
+    if (event.target == document.getElementById('myModal6')) {
+      closeModal6();
+    }
+  }
+
+  // Function to open the os linux 
+ 
+  function openModal7() {
+    document.getElementById('myModal7').style.display = "block";
+  }
+
+  // Function to close the modal
+  function closeModal7() {
+    document.getElementById('myModal7').style.display = "none";
+  }
+
+  // Close the modal if clicked outside of it
+  window.onclick = function(event) {
+    if (event.target == document.getElementById('myModal7')) {
+      closeModal7();
+    }
+  }
+
+   // Function to open the modal dbms
+ 
+  function openModal8() {
+    document.getElementById('myModal8').style.display = "block";
+  }
+
+  // Function to close the modal
+  function closeModal8() {
+    document.getElementById('myModal8').style.display = "none";
+  }
+
+  // Close the modal if clicked outside of it
+  window.onclick = function(event) {
+    if (event.target == document.getElementById('myModal8')) {
+      closeModal8();
+    }
+  }
+
+  // Function to open the modal Linux
+ 
+  function openModal9() {
+    document.getElementById('myModal9').style.display = "block";
+  }
+
+  // Function to close the modal
+  function closeModal9() {
+    document.getElementById('myModal9').style.display = "none";
+  }
+
+  // Close the modal if clicked outside of it
+  window.onclick = function(event) {
+    if (event.target == document.getElementById('myModal9')) {
+      closeModal9();
+    }
+  }
+
+    // Function to open the modal Linux
+ 
+  function openModal10() {
+    document.getElementById('myModal10').style.display = "block";
+  }
+
+  // Function to close the modal
+  function closeModal10() {
+    document.getElementById('myModal10').style.display = "none";
+  }
+
+  // Close the modal if clicked outside of it
+  window.onclick = function(event) {
+    if (event.target == document.getElementById('myModal10')) {
+      closeModal10();
+    }
+  }
